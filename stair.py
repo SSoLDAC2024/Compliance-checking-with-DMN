91f5f656db2ad88b30daa2a6794e6b4243db3fcc
rvrstrae <ruben.verstraeten@ugent.be>
2024-06-11 08:52:48 +0200

    Add files via upload

---
+++ stair.py
@@ -0,0 +1,7 @@
+class Stair:
+    def __init__(self, name, riserheight, threadlength, railings):
+        self.name = name
+        self.RiserHeight = riserheight
+        self.ThreadLength = threadlength
+        self.NumberOfRailings = railings
+
